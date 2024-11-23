3c7e284d8593d9a4db556ff5144636a3dcf1fbb4
Bashamega <adambashaahmednaji@gmail.com>
2024-11-23 12:32:10 +0200

    integration

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,34 +1,19 @@
 import { StyleSheet, View, Text, Button, Alert } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState } from 'react';
+import JokeDetails, { JokeProps } from '@/components/Joke'; // Import the Joke component
 
 export default function HomeScreen() {
-  const handlePress = async () => {
-    const jokeKey = 'joke_expires';
+  const [joke, setJoke] = useState<JokeProps | null>(null);
 
+  const fetchjoke = async () => {
     try {
-      // Retrieve the expiration time from AsyncStorage
-      const storedExpiration = await AsyncStorage.getItem(jokeKey);
-
-      if (storedExpiration) {
-        const expirationDate = new Date(storedExpiration);
-        const now = new Date();
-
-        // Check if the current time is past the expiration time
-        if (now < expirationDate) {
-          Alert.alert('Error', 'روح اشتغل يا فاضي!! تعالى لنا بكرة');
-          return;
-        }
-      }
-
-      // Set a new expiration date to midnight the next day
-      const newExpiration = new Date();
-      newExpiration.setHours(24, 0, 0, 0); // Midnight the next day
-      await AsyncStorage.setItem(jokeKey, newExpiration.toISOString());
-
-      // Show the joke or success message
-      alert('Button pressed! Joke generated.');
+      const response = await fetch('https://raw.githubusercontent.com/sa5afa/api/refs/heads/main/jokes.json'); // Replace with your actual API URL
+      const data = await response.json();
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomJoke = data[randomIndex];
+      setJoke(randomJoke); // Set joke to state
     } catch (error) {
-      console.error('Error handling joke press:', error);
+      console.error('Error fetching joke:', error);
       Alert.alert('Error', 'حدث خطأ، حاول مرة أخرى لاحقاً.');
     }
   };
@@ -36,7 +21,10 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>سخافة</Text>
-      <Button title="سخف عليا" onPress={handlePress} color="green" />
+      {joke && (
+        <JokeDetails author={joke.author} joke={joke.joke} categories={joke.categories} />
+      )}
+      <Button title="سخف عليا" onPress={fetchjoke} color="green" />
     </View>
   );
 }
